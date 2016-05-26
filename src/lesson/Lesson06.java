d672f8c53666b46c7dfd634b5eb1ae12161fe9e5
case2912 <shiroyama@maia.eonet.ne.jp>
2016-05-26 10:08:57 +0900

    modify Lesson06

--- src/lesson/Lesson06.java
+++ src/lesson/Lesson06.java
@@ -1,28 +1,31 @@
 package lesson;
+
 import lesson.Lesson03;
+
 public class Lesson06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double[] array = {1.0,-4.0,13.0/4.0,-3.0/4.0};
-		System.out.println(newton(-6, 0.001,100,array));
-		
+		double[] array = { 1.0, -4.0, 13.0 / 4.0, -3.0 / 4.0 };
+		System.out.println(newton(6, Math.pow(10, -7), 100, array));
+		System.out.println(newton(-6, Math.pow(10, -7), 100, array));
+
 	}
 
 	/*
 	 * Newton法 非線形方程式f(x)=0の解x=aを 数値的に求める反復法の一つ
 	 */
 	public static double newton(double x, double eps, int count, double[] array) {
-		int temp =0;
-		while (Math.abs(function(x, array)) > eps) {//残差を利用した例
+		int temp = 0;
+		while (Math.abs(function(x, array)) > eps) {// 残差を利用した例
 			x = x - function(x, array) / primeFunction(x, array);
 			temp++;
-			if(temp >= count){
+			if (temp >= count) {
 				System.out.println("Error has occurred! Count is " + temp);
 				return x;
 			}
 		}
-		System.out.println("Count is " + temp);
+		System.out.println("Count is " + temp + ". Result is");
 		return x;
 	}
 
@@ -33,8 +36,8 @@ public class Lesson06 {
 
 	public static double primeFunction(double x, double[] array) {
 		double temp = 0d;
-		for (int i = 1; i < array.length; i++) {
-			temp = temp * x + array[i]  * i;
+		for (int i = 0; i < array.length - 1; i++) {
+			temp = temp * x + array[i] * (double)(array.length - i - 1);
 		}
 		return temp;
 	}
